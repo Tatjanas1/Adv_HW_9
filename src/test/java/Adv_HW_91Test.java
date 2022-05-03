4767b7eb2c9f145d4ed83475575e9473e8ed0cc9
Tatjanas1 <tatiana.svistunova1@googlemail.com>
2022-05-03 15:05:46 +0300

    added dependency org.slf4j & added second test for Adv_HW_91

--- src/test/java/Adv_HW_91Test.java
+++ src/test/java/Adv_HW_91Test.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
@@ -7,5 +8,9 @@ public class Adv_HW_91Test {
     @org.testng.annotations.Test
     public void testAbbriv() {
         assertEquals(Adv_HW_91.abbriv("Kukushka", 3), "Kuk.");
+    }
+    @Test
+    public void testAbbriv2(){
+        assertEquals(Adv_HW_91.abbriv("Sergey", 4), "Serg.");
     }
 }
